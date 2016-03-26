8e7f03dd5ae6e0c7efa2f3b2f897b7a2131c6312
unknown <john.deyrup@gmail.com>
2016-03-26 17:16:04 -0400

    Finished movement

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,21 +4,33 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour {
     private Rigidbody rb;
     public float Speed;
-    public float JumpHeight;
+    public float JumpStrength;
     private bool alive;
+    private bool jumping;
 
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody>();
+        jumping = false;
 
     }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
-        Debug.Log(rb.position);
+        if (rb.velocity.y == 0) {
+            jumping = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) == true && !jumping)
+        {
+            Debug.Log("pressed a w");
+            jumping = true;
+            rb.velocity = new Vector3(moveHorizontal,  JumpStrength, 0);
+        }
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        
         rb.MovePosition(transform.position + movement * Speed);
 	}
 }

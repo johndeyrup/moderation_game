067034865bf97afa0baae2202ebb082046289acc
unknown <john.deyrup@gmail.com>
2016-03-27 16:28:21 -0400

    Added teleport on death and other things

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -7,13 +7,15 @@ public class PlayerControl : MonoBehaviour {
     public float JumpStrength;
     private bool alive;
     private bool jumping;
+    private bool grounded;
     public int JumpCollectables;
     public int SpeedCollectables;
-    public GameObject heroPrefab;
     private Vector3 checkpoint;
     private Camera cam;
     private Vector3 cameraOffset;
     private CapsuleCollider cap;
+    private SpriteAnimator anim;
+    private Canvas can;
 
 
     // Use this for initialization
@@ -25,6 +27,9 @@ public class PlayerControl : MonoBehaviour {
         cam = Camera.main;
         cameraOffset = new Vector3(0.0f, 0.0f, -6.0f);
         cap = GetComponent<CapsuleCollider>();
+        anim = GetComponent<SpriteAnimator>();
+        can = this.gameObject.transform.GetChild(0).GetComponent<Canvas>();
+
 
     }
 
@@ -33,14 +38,12 @@ public class PlayerControl : MonoBehaviour {
         Debug.Log(other.gameObject.tag);
         string objTag = other.gameObject.tag;
         if (objTag == "death_obj")
-        {            
-            Instantiate(heroPrefab, checkpoint, Quaternion.identity);
-            Destroy(gameObject);
+        {
+            rb.transform.position = checkpoint;
             
         }
         else if ( objTag == "spring_sprung")
         {
-            Destroy(other.gameObject);
             JumpStrength++;
             JumpCollectables++;
         }
@@ -53,27 +56,51 @@ public class PlayerControl : MonoBehaviour {
         else if (objTag =="Respawn")
         {
             checkpoint = other.transform.position;
+
+        }
+        else if (objTag =="Finish")
+        {
+            can.enabled = true;
         }
     }
 
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveHorizontal = Input.GetAxis("Horizontal")*Speed;
         RaycastHit hit;
-        if (Physics.SphereCast(rb.transform.position,  cap.radius/2, (-transform.up), out hit, .3f)) {
+        if (Physics.SphereCast(rb.transform.position,  cap.radius/2, (-transform.up), out hit, .4f)) {
             jumping = false;
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.W) == true && !jumping)
+        if (Input.GetKeyDown(KeyCode.W) == true && !jumping && grounded)
         {
             jumping = true;
             rb.velocity = new Vector3(moveHorizontal,  JumpStrength, 0);
         }
+        else
+        {
+            rb.velocity = new Vector3(moveHorizontal, rb.velocity.y, 0);
+        }
+        if (jumping == true)
+        {
+            anim.SetCurrentAnimation("jumping");
+        }
+        else if (rb.velocity.x != 0)
+        {
+            anim.SetCurrentAnimation("walking");
+        }
+        else
+        {
+            anim.SetCurrentAnimation("idle");
+        }
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
         
-        rb.MovePosition(transform.position + movement * Speed);
         cam.transform.position = rb.position + cameraOffset;
 	}
 }
